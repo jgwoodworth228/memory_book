b560070e7df5c45423fee440bb9bac72e25b0d7f
Jim Woodworth <jgwoodworth@gmail.com>
2015-11-18 18:05:58 -0600

    minor tweek to books controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -70,6 +70,6 @@ class BooksController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def book_params
-      params.require(:book).permit(:book_id, :user_id, :book_title, :title_page_image_url)
+      params.require(:book).permit(:user_id, :book_title, :title_page_image_url)
     end
 end
